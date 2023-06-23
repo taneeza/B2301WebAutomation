239de87d57c5b6894b672cca24ecfde5832325f9
tanija ferdousy <tanija.ferdousy@gmail.com>
2023-06-22 21:39:49 -0700

    2nd commit

--- src/test/java/com/izaanschool/heroku/HorizontalSliderTest.java
+++ src/test/java/com/izaanschool/heroku/HorizontalSliderTest.java
@@ -19,11 +19,11 @@ import java.util.concurrent.TimeUnit;
 
 
 /** problem statement
- * we want to navigate to Drag And Drop Elements page from herokuapp page and will see if
- * the drag and drop action works porperly or not
+ * we want to navigate to the  horizontal slider page page from herokuapp page and will see if
+ * the horizontal slides to the right or not
  * to do so,
  * 1. we will navigate to that page
- * 2.we will drag element A and release it on element B.
+ * 2.we will slide the slider from left to right and right to left
  * 3. we will check if the A element has been moved to element B and vice versa.
 
  */
@@ -32,7 +32,7 @@ import java.util.concurrent.TimeUnit;
 
 
 public class HorizontalSliderTest {
-    public static Logger logger = LogManager.getLogger(BasicAuthTest.class);
+    public static Logger logger = LogManager.getLogger(HorizontalSliderTest.class);
     WebDriver driver;
     String url = "http://the-internet.herokuapp.com/";
 
@@ -60,11 +60,14 @@ public class HorizontalSliderTest {
         Assert.assertEquals(h3actual, h3);
         logger.info("test passed");
 
+        //find the slider
         WebElement slider = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/input"));
 
+        //find the output element
         WebElement output = driver.findElement(By.id("range"));
         System.out.println(output.getText());
 
+        //drag action
         Actions act = new Actions (driver);
         act.dragAndDropBy(slider, 0, 0).perform();
 
@@ -93,6 +96,12 @@ public class HorizontalSliderTest {
 
         System.out.println("What Is The Output After Dragging Right? " + output.getText());
     }
+    @After
+    public void cleanup(){
+
+        driver.close();
+    }
+
 
 }
 
